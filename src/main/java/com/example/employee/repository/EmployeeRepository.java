ef37862ac19aef4d5a2dbbdd1db6d95688f7d8e7
ChengFeng AO <414356022@qq.com>
2018-05-03 19:32:22 +0800

    add EmployeeRepository

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,24 +1,38 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
+     public  Employee findTop1ByName(String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
+    public Employee findTop1ByNameContainingAndSalaryGreaterThan(String name,int salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
+     public Employee findTop1ByCompanyIdOrderBySalaryDesc(int companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
+      Page<Employee> findAll(Pageable pageable);
 
     //5.查找**的所在的公司的公司名称
-
+     //  Employee findById(int id);
+    public Employee findByName(String name);
     //6.将*的名字改成*,输出这次修改影响的行数
+     //Employee setName(String name);
 
     //7.删除姓名是*的employee
+    List<Employee> findAllByName(String name);
+    void deleteAllByName(String name);
 }
